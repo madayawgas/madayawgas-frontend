075c5cc599dc77bbdae4ad66c6f66fb8aa51992b
Leshka Alcontin <leshkarosealcontin@gmail,com>
2026-04-16 07:28:26 +0800

    updated user management ui

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -7,8 +7,8 @@ export default function Button({
   const baseStyles = "px-4 py-2 rounded-lg font-semibold flex items-center justify-center gap-2 transition disabled:opacity-50 disabled:cursor-not-allowed";
   
   const variants = {
-    primary: "bg-[#0F7AB2] text-white hover:bg-[#0c628f] shadow-sm",
-    secondary: "bg-gray-100 text-gray-700 hover:bg-gray-200 border border-gray-200",
+    primary: "bg-[#0F7AB2] text-white hover:bg-[#0c628f] shadow-sm border border-[#0A4B6E]",
+    secondary: "bg-gray-100 text-gray-700 hover:bg-gray-200 border border-gray-400",
     danger: "bg-red-50 text-red-600 hover:bg-red-100",
     outline: "bg-transparent border-2 border-[#0F7AB2] text-[#0F7AB2] hover:bg-[#F8FBFC]",
   };
