22481008ebe6942f4181984a98e218efaa00e7dc
Gianne-Pesana <gpesana@addu.edu.ph>
2026-04-15 14:48:42 +0800

    Modified router for easier testing

--- src/main.jsx
+++ src/main.jsx
@@ -24,18 +24,18 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
-   {
+  {
     path: "/",
-    element: <Navigate to="/login" replace />,
+    element: <Navigate to="/dashboard" replace />,
   },
   {
     path: "/",
     element: <Layout />,
     children: [
-      // {
-      //   index: true,
-      //   element: <Navigate to="/dashboard" replace />,
-      // },
+      {
+        index: true,
+        element: <Navigate to="/dashboard" replace />,
+      },
       {
         path: "dashboard",
         element: <Dashboard />,
